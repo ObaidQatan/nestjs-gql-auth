965a4427021fe6cda8383570f00a1f44b2876863
Obaid Qatan <ogaten27@gmail.com>
2022-09-02 00:27:13 +0530

    init

---
+++ src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      id: 1,
+      username: 'John Doe',
+      password: '123456',
+    },
+    {
+      id: 2,
+      username: 'Steve Smith',
+      password: '654321',
+    },
+  ];
+
+  create(createUserInput: CreateUserInput) {
+    const user = {
+      ...createUserInput,
+      id: this.users.length + 1,
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
+
+  // update(id: number, updateUserInput: UpdateUserInput) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
