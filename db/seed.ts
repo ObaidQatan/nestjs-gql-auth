ebf1f2a61124062ff284cd9602c04cab4ae22edb
Obaid Qatan <ogaten27@gmail.com>
2022-09-11 14:19:52 +0530

    remove seed data

--- db/seed.ts
+++ db/seed.ts
@@ -1,18 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
-  const user = await prisma.user.create({
-    data: {
-      username: 'John Doe',
-      password: bcrypt.hashSync('123456', 10),
-    },
-  });
-
-  console.log({ user });
+  // await prisma.user.deleteMany();
+  // const user = await prisma.user.create({
+  //   data: {
+  //     username: 'John Doe',
+  //     password: bcrypt.hashSync('123456', 10),
+  //   },
+  // });
+  // console.log({ user });
 }
 
 main()
