b7346691c56f0b968bc1179d2f022dbe9cd49d0c
Obaid Qatan <ogaten27@gmail.com>
2022-09-11 08:47:18 +0530

    update user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,52 +2,57 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import * as bcrypt from 'bcrypt';
 import { randomUUID } from 'crypto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      id: randomUUID({
-        disableEntropyCache: true,
-      }),
-      username: 'John Doe',
-      password: bcrypt.hashSync('123456', 10),
-    },
-    {
-      id: randomUUID({
-        disableEntropyCache: true,
-      }),
-      username: 'Steve Smith',
-      password: bcrypt.hashSync('654321', 10),
-    },
-  ];
+  constructor(private readonly prisma: PrismaService) {}
+  // private readonly users = [
+  //   {
+  //     id: randomUUID({
+  //       disableEntropyCache: true,
+  //     }),
+  //     username: 'John Doe',
+  //     password: bcrypt.hashSync('123456', 10),
+  //   },
+  //   {
+  //     id: randomUUID({
+  //       disableEntropyCache: true,
+  //     }),
+  //     username: 'Steve Smith',
+  //     password: bcrypt.hashSync('654321', 10),
+  //   },
+  // ];
 
   async create(createUserInput: CreateUserInput) {
-    const existUser = this.users.find(
-      (user) => user.username === createUserInput.username,
-    );
+    const existUser = await this.prisma.user.findUnique({
+      where: {
+        username: createUserInput.username,
+      },
+    });
     if (existUser) {
       throw new Error('User already exists');
     }
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(createUserInput.password, salt);
-    const user = {
-      username: createUserInput.username,
-      password: hashedPassword,
-      id: randomUUID({
-        disableEntropyCache: true,
-      }),
-    };
-    this.users.push(user);
-    return user;
+    return await this.prisma.user.create({
+      data: {
+        username: createUserInput.username,
+        password: hashedPassword,
+      },
+    });
   }
 
-  findAll() {
-    return this.users;
+  async findAll() {
+    return await this.prisma.user.findMany();
   }
 
-  findOne(username: string) {
-    const user = this.users.find((user) => user.username === username);
+  async findOne(username: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
     return user;
   }
 
