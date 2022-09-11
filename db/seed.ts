76e02a7dafb33a22398815ce3362c94153e098a6
Obaid Qatan <ogaten27@gmail.com>
2022-09-11 07:40:07 +0530

    prisma init

---
+++ db/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.deleteMany();
+  const user = await prisma.user.create({
+    data: {
+      username: 'John Doe',
+      password: bcrypt.hashSync('123456', 10),
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
